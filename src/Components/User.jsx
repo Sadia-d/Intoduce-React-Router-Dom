b723333739bcce7c72062c79cf77519c49339941
Sadia-d <subai>
2025-11-04 19:16:21 +0600

    useNavigate

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const User = ({user}) => {
 
-    const { id,name,email,username} = user;
+    const navigate = useNavigate()
+
+    const { id,name,email} = user;
+
+    const handleShowDetails = () =>{
+            navigate(`/user/${id}`)
+    }
+
     return (
         <div className='border border-blue-500 mb-2 p-4 rounded-xl'>
             <h1>{name}</h1>
             <p>{email}</p>
-            <Link to={`/user/${id}`} >Show Details</Link>  
+            <Link to={`/user/${id}`} >Show Details</Link> 
+            <br /> 
+            <button onClick={handleShowDetails} className='btn bg-blue-400 p-2 mt-2 text-white rounded-xl'  >click show details</button>
                  </div>
     );
 };
--- src/UserDeatils.jsx
+++ src/UserDeatils.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const UserDeatils = () => {
+
+    const navigate = useNavigate()
     const user = useLoaderData();
-     const { name,email,username} = user;
+    const { name, email } = user;
+
+    const goBack =()=>{
+        navigate(-1)
+    }
+
     return (
         <div>
-            <h1>Details the users </h1>
+            <div>
+                <h1>Details the users </h1>
                 <h2>{name}</h2>
                 <p>{email}</p>
+            </div>
+            <button onClick={goBack} className='btn bg-blue-400 p-2 mt-2 text-white rounded-xl' >Go back</button>
         </div>
     );
 };
